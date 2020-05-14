c8adb9f029931995fd42abd8d7f6614d67a614f2
John Wunderle <john.wunderle@us.sogeti.com>
2020-05-14 15:56:32 -0400

    weight form outline complete

--- src/WeightForm.js
+++ src/WeightForm.js
@@ -4,21 +4,39 @@ import moment from 'moment';
 import { withStyles } from '@material-ui/core/styles';
 import { Card, TextField, Button } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
+import SendIcon from '@material-ui/icons/Send';
 
 const styles = {
     root: {
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
+        // padding: '15px'
+        // justifyContent: 'center',
+        // alignItems: 'center'
     },
     formContainer: {
-        width: '800px',
         margin: '10px auto',
-        padding: '10px'
+        padding: '20px',
+        maxWidth: '400px'
+    },
+    titleText: {
+        fontSize: '1.25rem',
+        fontWeight: 'bold'
+    },
+    nameTextField: {
+        // width: '300px'
     },
     text: {
-        textAlign: 'left'
+        // marginRight: 'auto'
+        padding: '10px 0'
+    },
+    numContainer: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    submitButton: {
+        width: '150px',
+        marginLeft: 'auto'
     }
 }
 
@@ -32,7 +50,8 @@ class WeightForm extends React.PureComponent {
         ewOzs: 0,
         ewHalfOzs: 0,
         calcWeight: 0,
-        displayWeight: '0'
+        displayWeight: '0',
+        catName: ''
     }
 
     changeHandler = (e) => {
@@ -53,7 +72,7 @@ class WeightForm extends React.PureComponent {
             })
     }
 
-    handleChange = (e) => {
+    handleNumChange = (e) => {
         if (e.target.value !== "") {
             this.setState({ [e.target.name]: parseInt(e.target.value) })
         } else {
@@ -61,43 +80,9 @@ class WeightForm extends React.PureComponent {
         }
     }
 
-    // handleBlur = (e) => {
-    //     let inputNum = parseInt(e.target.value)
-    //     switch (e.target.name) {
-    //         case 'pounds':
-    //             console.log(inputNum)
-    //             console.log(this.state.pounds)
-    //             if (this.state.pounds === inputNum) {
-    //                 this.setState({ calcWeight: this.state.calcWeight + (inputNum * 16 * 10) })
-    //             }
-    //             break;
-    //         case 'ozs':
-    //             if (this.state.ozs !== inputNum) {
-    //                 this.setState({ calcWeight: this.state.calcWeight + (inputNum * 10) })
-    //             }
-    //             break;
-    //         case 'halfOzs':
-    //             if (this.state.halfOzs !== inputNum) {
-    //                 this.setState({ calcWeight: this.state.calcWeight + inputNum })
-    //             }
-    //             break;
-    //         case 'ewPounds':
-    //             if (this.state.ewPounds !== inputNum) {
-    //                 this.setState({ calcWeight: this.state.calcWeight - (inputNum * 16 * 10) })
-    //             }
-    //             break;
-    //         case 'ewOzs':
-    //             if (this.state.ewOzs !== inputNum) {
-    //                 this.setState({ calcWeight: this.state.calcWeight - (inputNum * 10) })
-    //             }
-    //             break;
-    //         case 'ewHalfOzs':
-    //             if (this.state.ewHalfOzs !== inputNum) {
-    //                 this.setState({ calcWeight: this.state.calcWeight - inputNum })
-    //             }
-    //             break;
-    //     }
-    // }
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
 
     handleSubmit = () => {
 
@@ -110,30 +95,34 @@ class WeightForm extends React.PureComponent {
 
         let leftOverHalfOzs = calcHalfOzs % 10
         let calcOzs = Math.floor(calcHalfOzs / 10)
-        this.setState({ displayWeight: `${calcOzs} ozs ${leftOverHalfOzs} hozs` })
+        let leftOverOzs = calcOzs % 16
+        let calcPounds = Math.floor(calcOzs / 16)
+        this.setState({ displayWeight: `${calcPounds} lbs ${leftOverOzs} ozs ${leftOverHalfOzs} hozs` })
     }
 
     render() {
-        const { cats, pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs, calcWeight, displayWeight } = this.state;
+        const { cats, pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs, calcWeight, displayWeight, catName } = this.state;
         const { classes } = this.props;
         return (
             <Card className={classes.formContainer}>
                 <form className={classes.root}>
+                    <Typography gutterBottom className={classes.titleText}>Weight Calculator</Typography>
+                    <TextField type='text' name='catName' value={catName} onChange={this.handleChange} label="Cat Name" variant="outlined" size="small" className={classes.nameTextField} />
                     <Typography gutterBottom className={classes.text}>Initial Weight</Typography>
-                    <div>
-                        <TextField type='number' name='pounds' value={pounds} onChange={this.handleChange} label="lbs" variant="outlined" size="small" />
-                        <TextField type='number' name='ozs' value={ozs} onChange={this.handleChange} label="ozs" variant="outlined" size="small" />
-                        <TextField type='number' name='halfOzs' value={halfOzs} onChange={this.handleChange} label="hozs" variant="outlined" size="small" />
+                    <div className={classes.numContainer}>
+                        <TextField type='number' name='pounds' value={pounds} onChange={this.handleNumChange} label="lbs" variant="outlined" size="small" onBlur={() => this.convertWeight(pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs)} />
+                        <TextField type='number' name='ozs' value={ozs} onChange={this.handleNumChange} label="ozs" variant="outlined" size="small" onBlur={() => this.convertWeight(pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs)} />
+                        <TextField type='number' name='halfOzs' value={halfOzs} onChange={this.handleNumChange} label="hozs" variant="outlined" size="small" onBlur={() => this.convertWeight(pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs)} />
                     </div>
                     <Typography gutterBottom className={classes.text}>Excess Weight</Typography>
-                    <div>
-                        <TextField type='number' name='ewPounds' value={ewPounds} onChange={this.handleChange} label="lbs" variant="outlined" size="small" />
-                        <TextField type='number' name='ewOzs' value={ewOzs} onChange={this.handleChange} label="ozs" variant="outlined" size="small" />
-                        <TextField type='number' name='ewHalfOzs' value={ewHalfOzs} onChange={this.handleChange} label="hozs" variant="outlined" size="small" />
+                    <div className={classes.numContainer}>
+                        <TextField type='number' name='ewPounds' value={ewPounds} onChange={this.handleNumChange} label="lbs" variant="outlined" size="small" onBlur={() => this.convertWeight(pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs)} />
+                        <TextField type='number' name='ewOzs' value={ewOzs} onChange={this.handleNumChange} label="ozs" variant="outlined" size="small" onBlur={() => this.convertWeight(pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs)} />
+                        <TextField type='number' name='ewHalfOzs' value={ewHalfOzs} onChange={this.handleNumChange} label="hozs" variant="outlined" size="small" onBlur={() => this.convertWeight(pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs)} />
                     </div>
                     <div className={classes.text}>Calculated Weight: {displayWeight}</div>
-                    <Button variant="contained" color="primary" onClick={() => this.convertWeight(pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs)}>Calculate</Button>
-                    <Button variant="contained" color="primary">Submit</Button>
+                    {/* <Button variant="contained" color="primary" onClick={() => this.convertWeight(pounds, ozs, halfOzs, ewPounds, ewOzs, ewHalfOzs)}>Calculate</Button> */}
+                    <Button endIcon={<SendIcon/>} variant="contained" color="primary" className={classes.submitButton}>Submit</Button>
                 </form>
             </Card>
         );
